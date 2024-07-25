d40c280651bead99efba988feab3bd2789142e83
Vikas Shah <vikasshah508@gmail.com>
2024-07-25 22:52:16 +0530

    change in frontend

---
+++ src/Utility/config.js
@@ -0,0 +1,4 @@
+
+module.exports = {
+    Base_URL:process.env.REACT_APP_API_URL    
+}
--- src/Utility/constant.js
+++
@@ -1,4 +0,0 @@
-// Define the base URL for the API
-module.exports = {
-    Base_URL:"http://localhost:5000/api/"    
-}
